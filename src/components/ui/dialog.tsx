de4750c71302d8f757b247e1009e1b4f83385953
KBRATIC <ederk608@gmail.com>
2026-08-13 14:09:23 -0300

    style: make dialog close button slightly more visible

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -44,7 +44,7 @@ const DialogContent = React.forwardRef<
       {...props}
     >
       {children}
-      <DialogPrimitive.Close className="absolute right-2 top-2 p-3 rounded-full opacity-70 ring-offset-background cursor-pointer transition-opacity hover:opacity-100 hover:bg-foreground/10 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
+      <DialogPrimitive.Close className="absolute right-2 top-2 p-2.5 rounded-full bg-foreground/5 opacity-80 border border-foreground/10 ring-offset-background cursor-pointer transition-all hover:bg-foreground/15 hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground">
         <X className="h-5 w-5" />
         <span className="sr-only">Close</span>
       </DialogPrimitive.Close>
