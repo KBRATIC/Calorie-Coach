c47fe03096adfc6f4349824d2c0eb1ab7d16b2df
KBRATIC <ederk608@gmail.com>
2026-08-19 11:28:02 -0300

    fix(ssr): remove duplicate nitro plugin to fix Vercel chunks

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,15 @@ import { tanstackStart } from '@tanstack/react-start/plugin/vite';
 import viteReact from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 import tailwindcss from "@tailwindcss/vite";
-import { nitro } from "nitro/vite";
 import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   plugins: [
-    tanstackStart(),
-    nitro({ preset: "vercel" }),
+    tanstackStart({
+      server: {
+        preset: "vercel",
+      }
+    }),
     viteReact(),
     tailwindcss(),
     tsconfigPaths(),
